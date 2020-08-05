9572518dcb70d833853a094a4671ac8e91935166
3198 <3198134437@qq.com>
2020-08-05 18:47:29 +0800

    你好，我自己

---
+++ dfdf/src/main/java/com/example/dfdf/DfdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.dfdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfdfApplication.class, args);
+	}
+
+}
